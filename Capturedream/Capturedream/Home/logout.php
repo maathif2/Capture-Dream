df94dcb2297932e5492676f72ccbe32e19ffaf88
Mohammed Aathif <IT20644680@my.sliit.lk>
2022-07-02 14:28:28 +0530

    Updated

--- Capturedream/Capturedream/Home/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 	
-
-    include "../config/config.php";
-    include '../config/core.php';
-    session_destroy();
-    echo "You have logged out successfully!<br/>";
-    header("Location:../Home/Home.php");
-
-?>
